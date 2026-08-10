2a9aeff0516a537a7185372440e2e4410b35a322
DamianMoniuszko <ddamian05@wp.pl>
2026-08-10 19:02:49 +0200

    Build FITBOOK features section

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Features from '../components/Features'
 
 function LandingPage() {
   return (
@@ -8,6 +9,7 @@ function LandingPage() {
 
       <main>
         <Hero />
+        <Features />
       </main>
     </>
   )
