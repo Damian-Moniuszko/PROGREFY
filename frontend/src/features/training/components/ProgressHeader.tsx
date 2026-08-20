b69b11507812d5f977bb622c5bc07460812083ec
Damian-Moniuszko <ddamian05@wp.pl>
2026-08-20 17:34:41 +0200

    refactor: add progress header component

---
+++ frontend/src/features/training/components/ProgressHeader.tsx
@@ -0,0 +1,11 @@
+const ProgressHeader = () => {
+  return (
+    <header className="training-progress__header">
+      <p className="training-progress__eyebrow">PROGRES</p>
+      <h1>Historia treningów</h1>
+      <p>Sprawdzaj wykonane treningi i zapisane wyniki.</p>
+    </header>
+  );
+};
+
+export default ProgressHeader;
